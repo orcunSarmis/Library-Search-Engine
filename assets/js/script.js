39fc0a2670e04a42fae48aa67d72083b0629157c
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-11 18:17:35 +1000

    Fetch request added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-window.addEventListener("load", function() {
-    fetch("https://www.loc.gov/newspapers/?fo=json").then(function(response){
-        console.log("Contente-type: ", response.headers.get("Content-Type"));
-        console.log("Status: ", response.status);
+var searchBtn = document.getElementById("searchBtn");
+
+function getLibrary() {
+
+var requestUrl = "https://www.loc.gov/newspapers/?fo=json";
+
+    fetch(requestUrl).then(function(response){
+        console.log( response);
+        return response.json();
+        
     })
-});
+    .then(function(data) {
+        console.log(data);
+
+        for (var i = 0; i < data.lenght; i++) {
+            var createTableRow = document.createElement("div");
+            // var 
+        }
+    })
+}
+// searchBtn.addEventListener("click", );
