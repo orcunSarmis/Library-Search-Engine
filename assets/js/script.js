70482e73d11d113dd3edf3d6058cf81bac2fea7d
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-11 22:18:08 +1000

    New url added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 var searchBtn = document.getElementById("searchBtn");
+var userInput = document.getElementById("userInput");
+var dropDown = document.getElementById("dropDown");
+
 
 function getLibrary() {
 
@@ -11,6 +14,9 @@ var requestUrl = "https://www.loc.gov/newspapers/?fo=json";
     })
     .then(function(data) {
         console.log(data);
+        
+var newUrl = "http://www.somewebsite.com/search?" + userInput + dropDown;
+location.replace(newUrl);
 
         for (var i = 0; i < data.lenght; i++) {
             var createTableRow = document.createElement("div");
